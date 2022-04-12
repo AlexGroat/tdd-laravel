34ed5264fac204039f8bab347391c3d2c0f84786
Alex Groat <alex.groat.95@gmail.com>
2022-04-12 15:28:15 +0930

    scoping projects timestamp 10:27

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use App\Models\Project;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -9,12 +10,20 @@ use Tests\TestCase;
 class ProjectTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     /** @test */
     public function test_project_has_path()
     {
         $project = Project::factory()->create();
 
         $this->assertEquals('/projects/' . $project->id, $project->path());
+    }
+
+    /** @test */
+    public function test_project_has_owner()
+    {
+        $project = Project::factory()->create();
+
+        $this->assertInstanceOf(User::class, $project->owner);
     }
 }
